43a55b53ed6229596728fa49500df2dfce1b07e8
Norman Basham <nbasham@yahoo.com>
2021-11-29 12:51:19 -0600

    Initial commit.

---
+++ TriviaEngWeek2021/PlayView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayView.swift
+//  TriviaEngWeek2021
+//
+//  Created by Norman Basham on 11/29/21.
+//
+
+import SwiftUI
+
+struct PlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayView()
+    }
+}
---
+++ TriviaEngWeek2021/TriviaEngWeek2021/TriviaEngWeek2021App.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaEngWeek2021App.swift
+//  TriviaEngWeek2021
+//
+//  Created by Norman Basham on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct TriviaEngWeek2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
