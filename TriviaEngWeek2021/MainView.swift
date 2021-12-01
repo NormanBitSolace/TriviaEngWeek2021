8d37f5ef35bb3ed957c01918ca4f72ce1839f1fb
Maribel Montejano <outdoorsole@users.noreply.github.com>
2021-12-01 08:19:23 -0800

    Add associated trivia category Int value for query

--- TriviaEngWeek2021/MainView.swift
+++ TriviaEngWeek2021/MainView.swift
@@ -7,31 +7,31 @@ struct RandomDogImageDataObject: Decodable {
 class MainViewModel: ObservableObject {
     @Published var isFetching = false
     @Published var randomDogImageDataObjects = [RandomDogImageDataObject]()
-    var categories = [
-        "General Knowledge",
-        "Entertainment: Books",
-        "Entertainment: Film",
-        "Entertainment: Music",
-        "Entertainment: Musicals & Theatres",
-        "Entertainment: Television",
-        "Entertainment: Video Games",
-        "Entertainment: Board Games",
-        "Science & Nature",
-        "Science: Computers",
-        "Science: Mathematics",
-        "Mythology",
-        "Sports",
-        "Geography",
-        "History",
-        "Politics",
-        "Art",
-        "Celebrities",
-        "Animals",
-        "Vehicles",
-        "Entertainment: Comics",
-        "Science: Gadgets",
-        "Entertainment: Japanese Anime & Manga",
-        "Entertainment: Cartoon & Animations"
+    var categories: [Int: String] = [
+        9: "General Knowledge",
+        10: "Entertainment: Books",
+        11: "Entertainment: Film",
+        12: "Entertainment: Music",
+        13: "Entertainment: Musicals & Theatres",
+        14: "Entertainment: Television",
+        15: "Entertainment: Video Games",
+        16: "Entertainment: Board Games",
+        17: "Science & Nature",
+        18: "Science: Computers",
+        19: "Science: Mathematics",
+        20: "Mythology",
+        21: "Sports",
+        22: "Geography",
+        23: "History",
+        24: "Politics",
+        25: "Art",
+        26: "Celebrities",
+        27: "Animals",
+        28: "Vehicles",
+        29: "Entertainment: Comics",
+        30: "Science: Gadgets",
+        31: "Entertainment: Japanese Anime & Manga",
+        32: "Entertainment: Cartoon & Animations"
     ]
 
     @MainActor
@@ -87,7 +87,7 @@ struct MainView: View {
                                     .frame(maxWidth: 100, maxHeight: 100, alignment: .center)
                                 }
 
-                                Text(mainViewModel.categories[number])
+                                Text(mainViewModel.categories[number + 9] ?? "")
                                     .font(.system(size: 14, weight: .medium))
                             }
                         }
