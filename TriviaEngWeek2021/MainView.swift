dac3eae16d46c8a59c825d44a8ce0ea2facf05ae
Maribel Montejano <outdoorsole@users.noreply.github.com>
2021-11-30 16:45:33 -0800

    Nest MainView within a NavigationView

--- TriviaEngWeek2021/MainView.swift
+++ TriviaEngWeek2021/MainView.swift
@@ -2,12 +2,14 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image("banner")
-            NavigationLink("Play", destination: PlayView())
-            NavigationLink("Options", destination: OptionsView())
+        NavigationView {
+            VStack {
+                Image("banner")
+                NavigationLink("Play", destination: PlayView())
+                NavigationLink("Options", destination: OptionsView())
+            }
+            .navigationTitle("Trivia")
         }
-        .navigationTitle("Trivia")
     }
 }
 
