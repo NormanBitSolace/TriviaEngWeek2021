5d95ac120d95c8ae2974bfbf70db333e14497461
Maribel Montejano <outdoorsole@users.noreply.github.com>
2021-11-30 21:36:27 -0800

    Replace the trivia banner with a random dog image within the MainView

--- TriviaEngWeek2021/MainView.swift
+++ TriviaEngWeek2021/MainView.swift
@@ -1,14 +1,49 @@
 import SwiftUI
 
+struct RandomDogImageDataObject: Decodable {
+    let message, status: String
+}
+
+class MainViewModel: ObservableObject {
+    @Published var isFetching = false
+    @Published var randomDogImageDataObject: RandomDogImageDataObject?
+
+    @MainActor
+    func fetchDogData() async {
+        isFetching = true
+        let urlString = "https://dog.ceo/api/breeds/image/random"
+        guard let url = URL(string: urlString) else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            self.randomDogImageDataObject = try JSONDecoder().decode(RandomDogImageDataObject.self, from: data)
+            isFetching = false
+        } catch {
+            isFetching = false
+            print("Error fetching data: \(error)")
+        }
+        
+    }
+}
+
 struct MainView: View {
+    @ObservedObject var mainViewModel = MainViewModel()
+
     var body: some View {
         NavigationView {
             VStack {
-                Image("banner")
+                if mainViewModel.isFetching {
+                    ProgressView()
+                } else {
+                    AsyncImage(url: URL(string: mainViewModel.randomDogImageDataObject?.message ?? ""))
+                }
                 NavigationLink("Play", destination: PlayView())
                 NavigationLink("Options", destination: OptionsView())
             }
             .navigationTitle("Trivia")
+            .task {
+                await mainViewModel.fetchDogData()
+            }
         }
     }
 }
